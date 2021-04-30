f99e6af546243d89b72e2be42c13dab556c57950
Kevin Eddie <keddie024@gmail.com>
2021-04-30 19:31:14 -0400

    updated models

--- models/Product.js
+++ models/Product.js
@@ -37,8 +37,9 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'Category',
+        model: 'category',
         key: 'id',
+        unique: false,
       }
     },
   },
